11120acc82efa0cb98afb155eee26624d4dd58af
mark <f18846188605@gmail.com>
2018-12-11 18:36:31 +0800

    init the project and primitive finish  read data from source file

---
+++ README.md
@@ -0,0 +1,7 @@
+### Introduction
+This is a project based on a realistic big data situation that given 10Gb size of TV ratings data to analise valuable information.
+
+The project use a big data computing framework called Spark.
+
+Java is used as the main programming language.
+
---
+++ src/main/java/main.java
@@ -0,0 +1,26 @@
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.TextInputFormat;
+import scala.Tuple2;
+
+import java.util.regex.Pattern;
+
+public class main {
+    public static void main(String[] args) {
+        String filePath = "./data.dat";
+//        String filePath = "./test.txt";
+        SparkConf conf = new SparkConf().setAppName("first").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        // read file use encoding format: GBK
+//        JavaRDD<String> fileRDD = sc.hadoopFile(filePath, TextInputFormat.class, LongWritable.class, Text.class).map(p -> new String(p._2.getBytes(), 0, p._2.getLength(), "GBK"));
+        JavaRDD<String> fileRDD = sc.textFile(filePath);
+        fileRDD = fileRDD.map(s->new String(s.getBytes(),0,s.length(),"gbk"));
+//        fileRDD.mapToPair(s -> new Tuple2(s, 1)).reduceByKey((a, b) -> Integer.getInteger(a.toString()) + Integer.getInteger(b.toString())).take(10).stream().forEach(System.out::println);
+//        fileRDD.mapToPair(s -> new Tuple2(s, 1)).reduce((a,b)->a).take(10).stream().forEach(System.out::println);
+//        fileRDD.map(s -> s.split("\\|")).map(s -> s[0]).take(10).stream().forEach(System.out::println);
+        fileRDD.take(10).stream().forEach(System.out::println);
+    }
+}
