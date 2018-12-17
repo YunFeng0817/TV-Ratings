a41380e55bf40fb193462f49e7015ab0698051f1
mark <f18846188605@gmail.com>
2018-12-17 12:17:23 +0800

    finish TV ratings' computation

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,7 +10,7 @@ import org.apache.spark.sql.*;
 
 import java.util.Objects;
 
-import static org.apache.spark.sql.functions.desc;
+import static org.apache.spark.sql.functions.*;
 
 public class main {
     public static void main(String[] args) throws AnalysisException {
@@ -25,14 +25,17 @@ public class main {
         // read file use encoding format: GBK
         JavaRDD<String> fileRDD = sc.hadoopFile(filePath, TextInputFormat.class, LongWritable.class, Text.class).map(p -> new String(p._2.getBytes(), 0, p._2.getLength(), "GBK"));
         JavaRDD<event> events = fileRDD.map(event::eventFactory).filter(Objects::nonNull);
-//        Dataset<event> eventsDataSet = spark.createDataset(events.collect(), Encoders.bean(event.class));
+        Dataset<event> eventsDataSet = spark.createDataset(events.collect(), Encoders.bean(event.class));
 //        eventsDataSet.createTempView("test");
         JavaRDD<channelEvent> channelEvents = events.filter(s -> s instanceof channelEvent).map(s -> (channelEvent) s);
         Dataset<channelEvent> channelEventsDS = spark.createDataset(channelEvents.collect(), Encoders.bean(channelEvent.class));
 //        channelEventsDS.groupBy("channel").count().sort(desc("count")).show();
 //        channelEventsDS.groupBy("show").count().sort(desc("count")).show();
 //        channelEvents.collect().stream().forEach(System.out::println);
-        channelEventsDS.groupBy("CACardID").count().show();
+        // compute the TV ratings
+        channelEventsDS.dropDuplicates("CACardID").groupBy("show").count().sort(desc("count")).show();
+//        eventsDataSet.groupBy("CACardID").count().sort(desc("count")).show();
+//        eventsDataSet.where("CACardID=825010226372496").sort("recordTime").show();
 //        channelEventsDS.printSchema();
     }
 }
