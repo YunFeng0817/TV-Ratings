b4cabb279860016a81b7870927807309c2302cd1
mark <f18846188605@gmail.com>
2018-12-20 01:04:32 +0800

    finish extracting valuable information from 10Gb size source files and add some comment

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,7 +7,6 @@ import java.sql.Timestamp;
 
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.*;
@@ -72,16 +71,29 @@ public class main {
         userRecords.show();
     }
 
+    /**
+     * extract valuable information data from source files and save as three files: './event','./quit','./channel'
+     *
+     * @param sparkSession     the spark session
+     * @param javaSparkContext the spark context in java
+     * @param filePath         the file path of the source files
+     */
     private static void readData(SparkSession sparkSession, JavaSparkContext javaSparkContext, String filePath) {
         // read file use encoding format: GBK
         JavaRDD<String> fileRDD = javaSparkContext.hadoopFile(filePath, TextInputFormat.class, LongWritable.class, Text.class).map(p -> new String(p._2.getBytes(), 0, p._2.getLength(), "GBK"));
         JavaRDD<event> events = fileRDD.map(event::eventFactory).filter(Objects::nonNull);
+        // cache the middle data ,events, to accelerate the procession
         events.persist(StorageLevel.MEMORY_AND_DISK());
+        // filter the event type , channelEvent , from events
         JavaRDD<channelEvent> channelEvents = events.filter(s -> s instanceof channelEvent).map(s -> (channelEvent) s);
+        // save channel events as sql table file(channel)
         sparkSession.createDataFrame(channelEvents, channelEvent.class).write().option("path", "./channel").mode(SaveMode.Overwrite).saveAsTable("data1");
+        // filter the event type , channelQuitEvent , from events
         JavaRDD<channelQuitEvent> channelQuitEvents = events.filter(s -> s instanceof channelQuitEvent).map(s -> (channelQuitEvent) s);
+        // save channel quit events as sql table file(quit)
         events.filter(s -> s instanceof channelQuitEvent).map(s -> (channelQuitEvent) s);
         sparkSession.createDataFrame(channelQuitEvents, channelQuitEvent.class).write().option("path", "./quit").mode(SaveMode.Overwrite).saveAsTable("data1");
+        // save events as sql table file(event)
         sparkSession.createDataFrame(events, event.class).write().option("path", "./event").mode(SaveMode.Overwrite).saveAsTable("data1");
         events.unpersist();
     }
