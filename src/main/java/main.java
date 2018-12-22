21f2bfe14237cc42966c1ad04a68451636e2323b
mark <f18846188605@gmail.com>
2018-12-22 20:14:48 +0800

    finish several basic data statistics and add the MLLib library

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,18 +1,22 @@
 import Event.channelEvent;
 import Event.channelQuitEvent;
 import Event.event;
-
-import java.util.Objects;
-import java.sql.Timestamp;
-
-import org.apache.hadoop.io.*;
+import avro.shaded.com.google.common.collect.Lists;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.*;
 import org.apache.spark.storage.StorageLevel;
+import scala.collection.Seq;
 
-import static org.apache.spark.sql.functions.*;
+import java.sql.Timestamp;
+
+import java.util.Objects;
+
+import static org.apache.spark.sql.functions.collect_list;
+import static org.apache.spark.sql.functions.desc;
 
 public class main {
     public static void main(String[] args) throws AnalysisException {
@@ -27,13 +31,14 @@ public class main {
         JavaSparkContext sc = JavaSparkContext.fromSparkContext(spark.sparkContext());
         sc.setLogLevel("WARN");
 //        readData(spark, sc, filePath);
-//        spark.read().load("./event").as(Encoders.bean(event.class));
-//        Dataset<channelEvent> channelEventDS = spark.read().load("./channel").as(Encoders.bean(channelEvent.class));
-//        channelEventDS.dropDuplicates("channel", "show").select("channel", "show").write().option("path", "./show").mode(SaveMode.Overwrite).saveAsTable("data1");
 //        generateSample(spark.read().load("./quit"), 0.00240582402);
+//        cleanData(spark);
+        getTVRatings(spark.read().load("./channel"), Timestamp.valueOf("2016-5-2 12:00:00"), Timestamp.valueOf("2016-5-2 14:00:00"));
+        getUserStatusTransform(spark, "825010214566974", Timestamp.valueOf("2016-5-2 00:00:00"), Timestamp.valueOf("2016-5-2 23:00:00"));
+        getWatchTime(spark.read().load("./sample"), "825010214566974", Timestamp.valueOf("2016-5-2 00:00:00"), Timestamp.valueOf("2016-5-2 23:00:00"));
+//        spark.read().load("./sample").groupBy("channel", "show").sum("lastTime").sort(desc("sum(lastTime)")).show(100);
+//        spark.read().load("./sample").select("CACardID").dropDuplicates("CACardID").sort("CACardID").show(100);
 
-
-//        getTVRatings(channelEventDS, Timestamp.valueOf("2016-5-2 12:00:00"), Timestamp.valueOf("2016-5-2 14:00:00"));
 //        getWatchTime(eventsDataSet, "825010304964177", Timestamp.valueOf("2016-1-1 12:00:00"), Timestamp.valueOf("2016-6-1 12:00:00"));
 //        getWatchTime(eventsDataSet, "825010385801697", Timestamp.valueOf("2016-1-1 12:00:00"), Timestamp.valueOf("2016-6-1 12:00:00"));
         spark.stop();
@@ -44,23 +49,45 @@ public class main {
      * @param startTime       the start time for TV ratings statistics
      * @param endTime         the end time for TV ratings statistics
      */
-    private static void getTVRatings(Dataset<channelEvent> channelEventsDS, Timestamp startTime, Timestamp endTime) {
+    private static void getTVRatings(Dataset<Row> channelEventsDS, Timestamp startTime, Timestamp endTime) {
         String timeFilter = "recordTime between '" + startTime.toString() + "' and '" + endTime + "'";
-        Dataset<channelEvent> totalUsers = channelEventsDS.dropDuplicates("CACardID").where(timeFilter);
+        Dataset<Row> totalUsers = channelEventsDS.where(timeFilter).dropDuplicates("CACardID");
         long userNum = totalUsers.count();
         totalUsers.groupBy("channel", "show").count().selectExpr("channel", "show", "count", "count/" + userNum).sort(desc("count")).show();
     }
 
     /**
-     * @param eventsDS
-     * @param CACardID
-     * @param startTime
-     * @param endTime
+     * @param eventsDS  the event DataSet
+     * @param CACardID  the specific user's CA card ID
+     * @param startTime the start time for TV ratings statistics
+     * @param endTime   the end time for TV ratings statistics
      */
-    private static void getWatchTime(Dataset<event> eventsDS, String CACardID, Timestamp startTime, Timestamp endTime) {
+    private static void getWatchTime(Dataset<Row> eventsDS, String CACardID, Timestamp startTime, Timestamp endTime) {
         String timeFilter = "recordTime between '" + startTime.toString() + "' and '" + endTime + "'";
-        Dataset<event> userRecords = eventsDS.where("CACardID=" + CACardID).where(timeFilter);
-        userRecords.show();
+        String CACardIDFilter = "CACardID=" + CACardID;
+        eventsDS.where(CACardIDFilter).where(timeFilter).show();
+        eventsDS.where(CACardIDFilter).where(timeFilter).groupBy("CACardID").sum("lastTime").sort(desc("sum(lastTime)")).show(100);
+    }
+
+    /**
+     * show the users' status transform procession order by record time
+     *
+     * @param sparkSession the spark session
+     * @param CACardID     the specific user's CA card ID
+     * @param startTime    the start time for TV ratings statistics
+     * @param endTime      the end time for TV ratings statistics
+     */
+    private static void getUserStatusTransform(SparkSession sparkSession, String CACardID, Timestamp startTime, Timestamp endTime) {
+        String timeFilter = "recordTime between '" + startTime.toString() + "' and '" + endTime + "'";
+        String CACardIDFilter = "CACardID=" + CACardID;
+        Dataset<Row> events = sparkSession.read().load("event").where(CACardIDFilter).where(timeFilter);
+        Dataset<Row> channel = sparkSession.read().load("channel").where(CACardIDFilter).where(timeFilter);
+        Dataset<Row> quit = sparkSession.read().load("quit").where(CACardIDFilter).where(timeFilter);
+        Seq<String> joinColumns1 = scala.collection.JavaConversions
+                .asScalaBuffer(Lists.newArrayList("CACardID", "recordTime", "typeID"));
+        Seq<String> joinColumns2 = scala.collection.JavaConversions
+                .asScalaBuffer(Lists.newArrayList("CACardID", "recordTime", "typeID", "channel", "show"));
+        events.join(channel, joinColumns1, "outer").join(quit, joinColumns2, "outer").sort("recordTime").show();
     }
 
     /**
@@ -72,6 +99,16 @@ public class main {
     private static void generateSample(Dataset<Row> events, double fraction) {
         Dataset<Row> userSample = events.select("CACardID").dropDuplicates("CACardID").sample(fraction);
         events.join(userSample, events.col("CACardID").equalTo(userSample.col("CACardID")), "leftsemi").sort("CACardID", "recordTime").write().option("path", "./sample").mode(SaveMode.Overwrite).saveAsTable("data1");
+    }
+
+    /**
+     * clean some impossible data from sample
+     *
+     * @param sparkSession the spark session
+     */
+    private static void cleanData(SparkSession sparkSession) {
+        Dataset<Row> data = sparkSession.read().load("./sample");
+        data.filter("lastTime<86400").write().option("path", "./sample1").mode(SaveMode.Overwrite).saveAsTable("data1");
     }
 
     /**
